033955763343bd009fe9a6ece6ea9dcabe7eb1f8
Danny <danny@skully.com>
2015-07-31 14:57:16 -0700

    Removed Angular.js

---
+++ Gruntfile.js
@@ -0,0 +1,8 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+
+  // Load the plugin that provides the "uglify" task.
+grunt.loadNpmTasks('grunt-uncss');
+  // Default task(s).
+};
