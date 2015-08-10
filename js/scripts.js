12d0b352d3d1876b061ca0d8bf888dab051669a2
Joshua Bloom <josh@skully.com>
2015-08-10 21:25:17 +0000

    Working on video fix

--- js/scripts.js
+++ js/scripts.js
@@ -680,13 +680,13 @@ function resizeVid() {
             var ratio = (vid.width() / vid.height());
             var section = vid.closest('section');
             if (section.width() > section.outerHeight()) {
-                vid.css('width', (section.width() * (ratio/2)));
-                // vid.css('margin-left', -((section.width() * ratio) / 4));
+                vid.css('width', (section.width() * ratio));
+                vid.css('margin-left', -((section.width() * ratio) / 4));
                 vid.css('height', 'auto');
             } else {
                 vid.css('width', 'auto');
-                // vid.css('height', (section.outerHeight() * ratio));
-                vid.css('margin-left', 0);
+                vid.css('height', (section.outerHeight() * ratio));
+                vid.css('margin-left', -((section.outerHeight() * ratio) / 4));
             }
     });
 
