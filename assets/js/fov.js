209a88ea66aef5a6a47f585f138efd6e7e30a94d
Danny <danny@skully.com>
2015-09-01 14:11:23 -0700

    Fixing jump

--- assets/js/fov.js
+++ assets/js/fov.js
@@ -1,9 +1,9 @@
-$(document).scroll(function() {
-  if ($(document).scrollTop() + $(window).height() > $('.fov-wrapper').offset().top) {
-    return setTimeout(function() {
-      $('#fov-toggle').prop('checked', true);
-      $('#mobile-skully-fov').prop('checked', true);
-      return $(document).off('scroll', scrollFn);
-    }, 1500);
-  }
-});
+// $(document).scroll(function() {
+//   if ($(document).scrollTop() + $(window).height() > $('.fov-wrapper').offset().top) {
+//     return setTimeout(function() {
+//       $('#fov-toggle').prop('checked', true);
+//       $('#mobile-skully-fov').prop('checked', true);
+//       return $(document).off('scroll', scrollFn);
+//     }, 1500);
+//   }
+// });
