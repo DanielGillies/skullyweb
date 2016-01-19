ff960a12db9890e75e508faa90eedbe2e2ab6c19
Danny <danny@skully.com>
2016-01-19 15:26:52 -0800

    FFFFF

--- assets/js/index.js
+++ assets/js/index.js
@@ -25,7 +25,6 @@ function test() {
 }
 
 //  var vid = document.getElementById("explosive-vid2")
-var hudvid = $("#hudvid")[0];
 // hudvid.pause();
 // hudvid.currentTime = 1;
 
@@ -37,7 +36,6 @@ var appTop = $('#app').offset().top;
 var appBot = appTop + $('#app').height();
 var signupTop = $('#signup').offset().top;
 var signupBot = signupTop + $('#signup').height();
-var hudTop = $('#hudvid').offset().top;
 var povTop = $('#pov').offset().top;
 var nextSectionTop = $('#explosive').next('section').offset().top;
 var hudPlaying = 0;
@@ -51,6 +49,21 @@ var forwards = true;
 // document.getElementsByClassName("explosive-active")[0].addEventListener('play', test);
 
 if (!window.mobilecheck()) {
+    document.getElementById("explosive").innerHTML = 
+        ' <div id="explosive-previous-holder"> <a id="explosive-previous" onclick="prevExplosion()"> <i class="icon-uparrow icon-md"></i> </a> </div> <div id="explosive-next-holder"> <a id="explosive-next" onclick="nextExplosion()"> <i class="icon-downarrow icon-md"></i> </a> </div> <video id="explosive-vid1" class="explosive-active"> <source class="lazy" data-src="http://dg6vcgn2vehm4.cloudfront.net/videos/Explosive1.webm" type="video/webm"> <source class="lazy" data-src="http://dg6vcgn2vehm4.cloudfront.net/videos/Explosive1.ogv" type="video/ogv"> <source class="lazy" data-src="http://dg6vcgn2vehm4.cloudfront.net/videos/Explosive1.mp4" type="video/mp4"> </video> <video id="explosive-vid2"> <source class="lazy" data-src="http://dg6vcgn2vehm4.cloudfront.net/videos/Explosive2.webm" type="video/webm"> <source class="lazy" data-src="http://dg6vcgn2vehm4.cloudfront.net/videos/Explosive2.ogv" type="video/ogv"> <source class="lazy" data-src="http://dg6vcgn2vehm4.cloudfront.net/videos/Explosive2.mp4" type="video/mp4"> </video> <video id="explosive-vid3"> <source class="lazy" data-src="http://dg6vcgn2vehm4.cloudfront.net/videos/Explosive3.webm" type="video/webm"> <source class="lazy" data-src="http://dg6vcgn2vehm4.cloudfront.net/videos/Explosive3.ogv" type="video/ogv"> <source class="lazy" data-src="http://dg6vcgn2vehm4.cloudfront.net/videos/Explosive3.mp4" type="video/mp4"> </video> <video id="explosive-vid4"> <source class="lazy" data-src="http://dg6vcgn2vehm4.cloudfront.net/videos/Explosive4.webm" type="video/webm"> <source class="lazy" data-src="http://dg6vcgn2vehm4.cloudfront.net/videos/Explosive4.ogv" type="video/ogv"> <source class="lazy" data-src="http://dg6vcgn2vehm4.cloudfront.net/videos/Explosive4.mp4" type="video/mp4"> </video> <video id="explosive-vid5"> <source class="lazy" data-src="http://dg6vcgn2vehm4.cloudfront.net/videos/Explosive5.webm" type="video/webm"> <source class="lazy" data-src="http://dg6vcgn2vehm4.cloudfront.net/videos/Explosive5.ogv" type="video/ogv"> <source class="lazy" data-src="http://dg6vcgn2vehm4.cloudfront.net/videos/Explosive5.mp4" type="video/mp4"> </video> <video id="explosive-vid1b"> <source class="lazy" data-src="http://dg6vcgn2vehm4.cloudfront.net/videos/Explosive1backwards.webm" type="video/webm"> <source class="lazy" data-src="http://dg6vcgn2vehm4.cloudfront.net/videos/Explosive1backwards.ogv" type="video/ogv"> <source class="lazy" data-src="http://dg6vcgn2vehm4.cloudfront.net/videos/Explosive1backwards.mp4" type="video/mp4"> </video> <video id="explosive-vid2b"> <source class="lazy" data-src="http://dg6vcgn2vehm4.cloudfront.net/videos/Explosive2backwards.webm" type="video/webm"> <source class="lazy" data-src="http://dg6vcgn2vehm4.cloudfront.net/videos/Explosive2backwards.ogv" type="video/ogv"> <source class="lazy" data-src="http://dg6vcgn2vehm4.cloudfront.net/videos/Explosive2backwards.mp4" type="video/mp4"> </video> <video id="explosive-vid3b"> <source class="lazy" data-src="http://dg6vcgn2vehm4.cloudfront.net/videos/Explosive3backwards.webm" type="video/webm"> <source class="lazy" data-src="http://dg6vcgn2vehm4.cloudfront.net/videos/Explosive3backwards.ogv" type="video/ogv"> <source class="lazy" data-src="http://dg6vcgn2vehm4.cloudfront.net/videos/Explosive3backwards.mp4" type="video/mp4"> </video> <video id="explosive-vid4b"> <source class="lazy" data-src="http://dg6vcgn2vehm4.cloudfront.net/videos/Explosive4backwards.webm" type="video/webm"> <source class="lazy" data-src="http://dg6vcgn2vehm4.cloudfront.net/videos/Explosive4backwards.ogv" type="video/ogv"> <source class="lazy" data-src="http://dg6vcgn2vehm4.cloudfront.net/videos/Explosive4backwards.mp4" type="video/mp4"> </video> <video id="explosive-vid5b"> <source class="lazy" data-src="http://dg6vcgn2vehm4.cloudfront.net/videos/Explosive5backwards.webm" type="video/webm"> <source class="lazy" data-src="http://dg6vcgn2vehm4.cloudfront.net/videos/Explosive5backwards.ogv" type="video/ogv"> <source class="lazy" data-src="http://dg6vcgn2vehm4.cloudfront.net/videos/Explosive5backwards.mp4" type="video/mp4"> </video>'
+
+        document.getElementById("indexhero").innerHTML = 
+        '<video autoplay loop class="fillWidth"> <source src="http://dg6vcgn2vehm4.cloudfront.net/videos/indexvid.mp4" type="video/mp4"> <source src="http://dg6vcgn2vehm4.cloudfront.net/videos/indexvid.webm" type="video/webm"> <source src="http://dg6vcgn2vehm4.cloudfront.net/videos/indexvid.ogv" type="video/ogv"> Your browser does not support the video tag. I suggest you upgrade your browser. </video>'
+
+        document.getElementById("hud").innerHTML = '<video id="hudvid" muted style="height:100%; position:absolute; right: 0; top: 0;"> <source id="hudwebm" class="lazy" data-src="http://dg6vcgn2vehm4.cloudfront.net/videos/HUD1.webm" type="video/webm"> <source id="hudmp4" class="lazy" data-src="http://dg6vcgn2vehm4.cloudfront.net/videos/HUD1.mp4" type="video/mp4"> <source id="hudogv" class="lazy" data-src="http://dg6vcgn2vehm4.cloudfront.net/videos/HUD1.ogv" type="video/ogv"> </video> <div class="container" id="hudtext"> <div id="hudheader" class="mb56 row"> <div class="col-md-12"> <h2 class="xbold uppercase">360&deg; Awareness, <br>Every Second</h2> </div> </div> <div class="hudbody row"> <div class="col-md-4"> <h5 class="uppercase xbold"> <i class="icon-HUD_3 icon-sm2"></i> Heads-Up Display</h5> <p class="subheader">Always in focus and floating <br>right in front of you.</p> <hr> </div> </div> <div class="hudbody row"> <div class="col-md-4"> <h5 class="uppercase xbold"> <i class="icon-blindspot_w2 icon-sm2"></i> 180° Blindspot Camera</h5> <p class="subheader">Ultra-wide to show you more <br> than conventional mirrors.</p> <hr> </div> </div> <div class="mb32" id="hudbuttons"> <a class="btn btn-hud active-hud" onclick="swapHUD()"></a> <a class="btn btn-hud" onclick="swapHUD()"></a> </div> </div> <div class="container" id="hudtext2"> <div class="row mb56"> <div class="col-md-12"> <h2 class="xbold uppercase">Vital, Real-time Data <br> at a Glance</h2> </div> </div> <div class="row"> <div class="col-md-4"> <h5 class="xbold uppercase"> <i class="icon-mph_w2 icon-sm2"></i> Speed</h5> <p class="subheader">Instantly glanceable MPH in <br>your Heads-Up Display. </p> <hr> </div> </div> <div class="row"> <div class="col-md-4"> <h5 class="bold uppercase"> <i class="icon-checktech_w2 icon-sm2"></i> Turn-by-Turn GPS</h5> <p class="subheader">Audio and visual navigation <br>when you need it.</p> <hr> </div> </div> <a class="btn btn-hud" onclick="swapHUD()"></a> <a class="btn btn-hud active-hud" onclick="swapHUD()"></a> </div>'
+
+        document.getElementById("pov-vid").innerHTML = '<video autoplay loop class="fillWidth"> <source src="http://dg6vcgn2vehm4.cloudfront.net/videos/pov.webm" type="video/webm"> <source src="http://dg6vcgn2vehm4.cloudfront.net/videos/pov.ogv" type="video/ogv"> <source src="http://dg6vcgn2vehm4.cloudfront.net/videos/pov.mp4" type="video/mp4"> </video>'
+
+        document.getElementById("pov").addClass('video-background')
+
+        var hudTop = $('#hudvid').offset().top;
+        var hudvid = $("#hudvid")[0];
+
     $(window).scroll(function() { // assign scroll event listener
         var currentScroll = $(window).scrollTop(); // get current position
         if (currentScroll >= explosiveTop - 55 && currentScroll <= explosiveBot && !inExplosive) {
@@ -69,59 +82,6 @@ if (!window.mobilecheck()) {
             });
             $("#hudvid").on("ended", swapHUD);
         }
-
-        // if (currentScroll >= povTop - 55 && currentScroll <= explosiveTop - 55 && povComplete === 0) {
-        //     povComplete = 1;
-        //     TweenLite.set(".hud-animation", {
-        //         perspective: 500
-        //     });
-        //     TweenMax.to("#hud-bg", 1.5, {
-        //         rotationY: 20,
-        //         transformOrigin: "left 50%",
-        //         left: "-=20%",
-        //         opacity: .7,
-        //         ease: Power1.easeInOut
-        //     });
-        //     TweenMax.to("#hud-nose", 1.5, {
-        //         rotationY: 20,
-        //         transformOrigin: "left 50%",
-        //         left: "-=20%",
-        //         opacity: .9,
-        //         ease: Power1.easeInOut
-        //     });
-        //     TweenMax.to("#hud-rear", 1.5, {
-        //         rotationY: 20,
-        //         transformOrigin: "left 50%",
-        //         left: "-=20%",
-        //         ease: Power1.easeInOut
-        //     });
-        //     TweenMax.to("#hud-mph", 1.5, {
-        //         rotationY: 20,
-        //         transformOrigin: "left 50%",
-        //         left: "-=20%",
-        //         ease: Power1.easeInOut
-        //     });
-
-        //     TweenMax.to("#hud-nose", 1, {
-        //         left: "+=1%",
-        //         delay: 1,
-        //         ease: Power2.easeInOut
-        //     });
-        //     TweenMax.to("#hud-rear", 1.5, {
-        //         scale: 2,
-        //         top: "-=35%",
-        //         left: "-=55%",
-        //         ease: Power2.easeInOut,
-        //         delay: .5
-        //     });
-        //     TweenMax.to("#hud-mph", 1.5, {
-        //         scale: 2.5,
-        //         top: "-=55%",
-        //         left: "-=80%",
-        //         ease: Power2.easeInOut,
-        //         delay: .5
-        //     });
-        // }
     })
 }
 
